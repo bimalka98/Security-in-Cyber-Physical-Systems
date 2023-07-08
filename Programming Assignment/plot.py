710bdd623524fe9db4da2b36729aaf70057347cb
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2023-07-08 21:38:14 +0530

    add plot

--- Programming Assignment/plot.py
+++ Programming Assignment/plot.py
@@ -9,9 +9,24 @@ p = np.array([0.54, 0.53, 0.53, 0.57, 0.58])
 t_lookup = np.array([0.11972, 0.03027, 0.00837, 0.00207, 0.00059])
 
 # Plotting the data
-plt.plot(trial, t_lookup)
-plt.xscale('log')
-plt.xlabel('Number of Chains (m)')
-plt.ylabel('Time per Lookup (t/lookup)')
+
+# make the figure size 4:3 ratio
+plt.figure(figsize=(12, 9))
+
+# Plotting the time per lookup in scatter plot, connecting the points using a dashed line
+plt.plot(trial, t_lookup, 'o--', label='Time per Lookup', color='red', linewidth=2, markersize=12)
+
+# x ticks must be integers
+plt.xticks(trial)
+
+# activate the grid
+plt.grid()
+
+plt.xlabel('Trial number')
+plt.ylabel('Time per Lookup (s)')
 plt.title('Rainbow Table Lookup Attack Experiment')
+
+# Saving the plot with maximum quality
+plt.savefig('plot.png', dpi=1000)
+
 plt.show()
